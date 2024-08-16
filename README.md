bb68adbb94b903610e511dc55c1721ac7bc32ef8
Geoffrey Omollo <geoffreyomollo001@gmail.com>
2024-08-16 11:16:45 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Fullstack Bank Application
+
+This is a fullstack bank application built using ASP.NET Web API & Angular
+
+## Features
+
+- Create a new account
+
+
+
---
+++ backend/WebAPI/WebAPI/Database/APIDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Database
+{
+    public class APIDBContext : DbContext
+    {
+        public DbSet<Bank> Banks { get; set; }
+        public DbSet<BankAccount> BankAccounts { get; set; }
+
+        public APIDBContext(DbContextOptions<APIDBContext> options) : base(options)
+        {
+            
+        }
+    }
+}
